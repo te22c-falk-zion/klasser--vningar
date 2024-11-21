030db27beb9fd87c7f71811239d8b5ef7006dd6d
Zion Falk <zion.falk@elev.ga.ntig.se>
2024-11-21 15:56:02 +0100

    cleaning

--- klasser övningar/Program.cs
+++ klasser övningar/Program.cs
@@ -1,11 +1,8 @@
-﻿
-
-using System.Threading.Tasks.Dataflow;
+﻿using System.Threading.Tasks.Dataflow;
 
 book b1 = new book("a", 5 );
 book b2 = new book("b", 10);
 book b3 = new book("c", 15);
-
 
 Console.WriteLine($"{b3.GetName()} and {b3.GetCurrentPage()}");
 b3.turnpage();
